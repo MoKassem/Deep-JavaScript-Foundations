636dacbb4e89c856f9a4bdf76a9de4f37ae7014e
Mohamed Kassem <mohamed.kassem@skipthedishes.ca>
2018-05-20 15:37:12 -0600

    testing the strict and unstrict mode

---
+++ strict.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+function mystrict(){
+  bar = 5;
+  console.log(bar);
+}
+
+
+mystrict(); // will throw an errormystrict
---
+++ unstrict.js
@@ -0,0 +1,7 @@
+function myunstrict() {
+  bar = 5;
+  console.log(bar);
+}
+
+
+myunstrict(); // will not throw an error. will create a variable in the global scope, and assign it a value of 5.
