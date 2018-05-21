f43835fbd68b2c494f0ef1f18e0ccb5ff67e1073
Mohamed Kassem <mohamed.kassem@skipthedishes.ca>
2018-05-21 12:42:31 -0600

    simple scripts to compare between the strict and non-strict mode

--- strict.js
+++ strict.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 function mystrict(){
   bar = 5;
   console.log(bar);
@@ -8,3 +7,19 @@ function mystrict(){
 
 
 mystrict(); // will throw an errormystrict
+
+/*
+
+ReferenceError: bar is not defined
+    at mystrict (/Users/mkassem/Desktop/FrontEndMasterCourses/Deep-JavaScript-Foundations/strict.js:5:7)
+    at Object.<anonymous> (/Users/mkassem/Desktop/FrontEndMasterCourses/Deep-JavaScript-Foundations/strict.js:10:1)
+    at Module._compile (module.js:573:32)
+    at Object.Module._extensions..js (module.js:582:10)
+    at Module.load (module.js:490:32)
+    at tryModuleLoad (module.js:449:12)
+    at Function.Module._load (module.js:441:3)
+    at Module.runMain (module.js:607:10)
+    at run (bootstrap_node.js:420:7)
+    at startup (bootstrap_node.js:139:9)
+
+*/
--- unstrict.js
+++ unstrict.js
@@ -3,5 +3,4 @@ function myunstrict() {
   console.log(bar);
 }
 
-
 myunstrict(); // will not throw an error. will create a variable in the global scope, and assign it a value of 5.
